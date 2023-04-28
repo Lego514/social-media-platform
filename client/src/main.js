53b5ea1940353859ec2c86a27902384cd84dd918
Lego514 <ray1998514@gmail.com>
2023-04-27 19:23:14 -0700

    Setup register page and router

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/styles.css'
+import router from './router.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
+
+
+
